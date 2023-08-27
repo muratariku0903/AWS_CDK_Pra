ee42441d4b0012b42c36b057fe6f4ae8d1dd802c
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-08-27 11:17:35 +0900

    first commit

---
+++ cdk-p-s3/test/cdk-p-s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPS3 from '../lib/cdk-p-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-p-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPS3.CdkPS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
