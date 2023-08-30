de0bea78225f86cc08a132c651f4d232695d5976
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-08-30 20:03:29 +0900

    wip

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs'
 // import * as s3 from 'aws-cdk-lib/aws-s3'
 // import * as db from 'aws-cdk-lib/aws-dynamodb'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as cloudWatch from 'aws-cdk-lib/aws-cloudwatch'
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -32,5 +33,13 @@ export class InfraStack extends cdk.Stack {
       functionName: 'murataTestFunction',
     })
     console.log(func)
+
+    // cloudwatch for lambda
+    const watch = new cloudWatch.Alarm(this, 'murataTestCloudWatch', {
+      evaluationPeriods: 1,
+      threshold: 1,
+      metric: func.metricErrors(),
+    })
+    console.log(watch)
   }
 }
