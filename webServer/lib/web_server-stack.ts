c61df9213dc3ec8fa639bca4ff425fd614d6d52d
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-09-04 23:03:49 +0900

    wip

--- webServer/lib/web_server-stack.ts
+++ webServer/lib/web_server-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import { readFileSync } from 'fs'
 
 export class WebServerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -21,5 +22,17 @@ export class WebServerStack extends cdk.Stack {
       allowAllOutbound: true,
     })
     sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), sgName)
+
+    // EC2
+    const murataEc2 = new ec2.Instance(this, 'murataWebServerEC2Id', {
+      vpc: vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      securityGroup: sg,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
+      keyName: 'murata-key-02',
+    })
+    const userData = readFileSync('./lib/userdata.sh', 'utf-8')
+    murataEc2.addUserData(userData)
   }
 }
